e8a38cd8644531390415c59e977a785fe1d03a4e
Zelchi <tininhakawaii@gmail.com>
2025-12-01 18:27:20 -0300

    init: start my libary test in go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zelchi/testego
+
+go 1.25.4
---
+++ main.go
@@ -0,0 +1,13 @@
+package testego
+
+import "fmt"
+
+func Laranja(boleano bool) {
+	if boleano {
+		fmt.Println("LARANJA!!!")
+	} else {
+		fmt.Println("LIMÃO!!!")
+	}
+}
+
+// Teste
