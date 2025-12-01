5135d60726ddba6b7ed61f1319b3b1a3bd5b3d30
Zelchi <tininhakawaii@gmail.com>
2025-12-01 20:54:14 -0300

    feat: implemented methods and type/interface

--- main.go
+++ main.go
@@ -2,12 +2,31 @@ package testego
 
 import "fmt"
 
-func Laranja(boleano bool) {
-	if boleano {
-		fmt.Println("LARANJA!!!")
-	} else {
-		fmt.Println("LIMÃO!!!")
+type Fruit struct {
+	Name  string
+	Price float64
+}
+type Market interface {
+	Selling() bool
+	Marketing() bool
+}
+
+func (fruit Fruit) Selling() {
+	fmt.Println("Selling: ", fruit.Name)
+}
+
+func (fruit Fruit) Marketing() {
+	fmt.Println("Marketing: ", fruit.Price)
+}
+
+func CreateFruit(name string, price float64) Fruit {
+	return Fruit{
+		Name:  name,
+		Price: price,
 	}
 }
 
-// Teste
+func Start(fruit Fruit) {
+	fruit.Selling()
+	fruit.Marketing()
+}
